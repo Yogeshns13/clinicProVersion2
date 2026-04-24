0ed02ec613e82b567c7c953472ee371590844973
yogesh <nsyogeshofficial@gmail.com>
2026-04-24 16:01:14 +0530

    menu bar updated

--- src/Consultation/ConsultationList.jsx
+++ src/Consultation/ConsultationList.jsx
@@ -1,4 +1,3 @@
-// src/components/ConsultationList.jsx
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FiSearch, FiX, FiPlus, FiCalendar, FiFileText } from 'react-icons/fi';
